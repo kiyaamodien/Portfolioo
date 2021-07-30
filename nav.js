72de34eac0a704416766deae19f6b64195dd2701
kiyaamodien <kiyaamudienkhan@gmail.com>
2021-07-30 14:39:19 +0200

    update

--- nav.js
+++ nav.js
@@ -1,3 +1,3 @@
-function toggleNavbar() {
-  document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
-}
+// function toggleNavbar() {
+//   document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
+// }
